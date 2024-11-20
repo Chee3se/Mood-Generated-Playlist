ba4ed37172152fb8141f9e56fd9b4710baf24cf9
eaterz <muraskoruslans@gmail.com>
2024-11-20 10:50:50 +0200

    button style

--- resources/js/Components/VideoControl.jsx
+++ resources/js/Components/VideoControl.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function VideoControl({ onStart }) {
     return (
         <div className="video-control">
-            <button onClick={onStart} className="px-4 py-2 bg-blue-500 text-white rounded-lg">
+            <button
+                onClick={onStart}
+                className="px-4 py-2 bg-green-500 hover:bg-green-400 text-white rounded-full font-semibold transition-colors duration-200 focus:outline-none"
+            >
                 Start Detecting
             </button>
         </div>
